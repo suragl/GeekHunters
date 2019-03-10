089a805e27687d3ab786c3c4cb052d88615bd106
suragl <sur.agl.85@gmail.com>
2019-03-10 17:24:25 +1300

    test webapi via postman

--- Surabhi-GeekHunter/GeekHunter/GeekHunter/Data/GeekHunterContext.cs
+++ Surabhi-GeekHunter/GeekHunter/GeekHunter/Data/GeekHunterContext.cs
@@ -9,14 +9,35 @@ namespace GeekHunter.Data
 {
     public class GeekHunterContext : DbContext, IDbContext
     {
-        public GeekHunterContext (DbContextOptions<GeekHunterContext> options)
-            : base(options)
+        private static bool _created = false;
+        public GeekHunterContext()
         {
+            if (!_created)
+            {
+                _created = true;
+                Database.EnsureCreated();
+                Database.Migrate();
+            }
         }
 
         public DbSet<Candidate> Candidates { get; set; }
         public DbSet<Skill> Skills { get; set; }
         public DbSet<CandidateSkillMap> CandidateSkillMap { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source = Database/GeekHunter.sqlite ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CandidateSkillMap>()
+                .HasKey(cs => new { cs.CandidateRefId, cs.SkillRefId });
+        }
+
     }
 }
+
